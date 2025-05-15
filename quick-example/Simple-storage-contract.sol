8e7895c891e20491638e39e03714d1ce4b453f16
Nathan Rossow <nathanrossow@gmail.com>
2025-05-15 13:44:31 -0700

    Create Simple-storage-contract.sol

---
+++ quick-example/Simple-storage-contract.sol
@@ -0,0 +1,7 @@
+// Simple storage contract
+pragma solidity ^0.8.0;
+contract Storage {
+    uint256 public value;
+    function set(uint256 _value) public { value = _value; }
+    function get() public view returns (uint256) { return value; }
+}
